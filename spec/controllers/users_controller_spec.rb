80b57d496678079c59c40d4a10ee53f26043db28
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-02-10 15:21:12 +1300

    Fixed rubocop error.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,17 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
-  context 'user is signed in' do
+  context "user is signed in" do
     before { sign_in user }
-    describe 'GET #index' do
+    describe "GET #index" do
       before { get :index }
       it { expect(response).to be_success }
     end
   end
 
-  context 'Not signed in' do
-    describe 'GET index' do
+  context "Not signed in" do
+    describe "GET index" do
       before { get :index }
       it { expect(response).to redirect_to(user_session_path) }
     end
