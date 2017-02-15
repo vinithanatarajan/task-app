472f309fd2124ba20bde7974d4ffc1f3941d6fe8
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-02-16 12:20:28 +1300

    Added tests for editing a student.

--- spec/features/students_spec.rb
+++ spec/features/students_spec.rb
@@ -1,15 +1,33 @@
 require "rails_helper"
 
-RSpec.feature User, type: :feature do
+RSpec.feature User, type: :feature, js: true do
   let!(:student) { FactoryGirl.create :student }
 
   context "can see list of student" do
     it "shows the list" do
+      # binding.pry
       visit "/students"
       expect(page).to have_text("Students page")
       expect(page).to have_text(student.name)
       expect(page).to have_text(student.class_room)
       expect(page).to have_text(student.current_level)
+    end
+  end
+
+  describe "I can add student" do
+    before do
+      visit "/students"
+      click_on "Add a Student"
+      fill_in "Name", with: "Peter"
+      fill_in "Class room", with: "Year 8"
+      fill_in "Current level", with: "At Expected level"
+      click_on "Save"
+    end
+
+    it "I can see the new student on the page" do
+      expect(page).to have_content("Peter")
+      expect(page).to have_content("Year 8")
+      expect(page).to have_content("At Expected level")
     end
   end
 
@@ -27,6 +45,18 @@ RSpec.feature User, type: :feature do
       expect(page).to have_content("Molly")
       expect(page).to have_content("Year 8")
       expect(page).to have_content("At Expected level")
+    end
+  end
+
+  describe "I can delete the student" do
+    before do
+      visit "/students"
+      click_on "Delete"
+      page.driver.browser.switch_to.alert.accept
+    end
+
+    it "I no longer have student on the page" do
+      expect(page).to change { Student.count }.by(-1)
     end
   end
 end
