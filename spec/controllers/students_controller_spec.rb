c30e872d99946ca2b614240ed657a707fcf9a524
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-02-15 17:35:43 +1300

    Writing feature specs for student controller.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,4 +1,9 @@
 require "rails_helper"
 
 RSpec.describe StudentsController, type: :controller do
+  let(:student) { FactoryGirl.create(:student) }
+  describe "GET #index" do
+    before { get :index }
+    it { expect(response).to be_success }
+  end
 end
--- spec/features/students_spec.rb
+++ spec/features/students_spec.rb
@@ -12,4 +12,21 @@ RSpec.feature User, type: :feature do
       expect(page).to have_text(student.current_level)
     end
   end
+
+  describe "I can edit the student details" do
+    before do
+      visit "/students"
+      click_on "Edit"
+      fill_in "Name", with: "Molly"
+      fill_in "Class room", with: "Year 8"
+      fill_in "Current level", with: "At Expected level"
+      click_on "Save"
+    end
+
+    it "I can see that the student details are updated" do
+      expect(page).to have_content("Molly")
+      expect(page).to have_content("Year 8")
+      expect(page).to have_content("At Expected level")
+    end
+  end
 end
