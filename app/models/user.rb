540a51261ce0ddf2331e0cbce2dd5946135b3398
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-02-16 15:16:16 +1300

    Added associations for roles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many   :students, through: :class_rooms
+  has_many   :user_roles
+  has_many   :roles, through: :user_roles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
