30dcc4ecd7c4619d86ad2d15499d777f2439b988
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-02-21 09:45:55 +1300

    fixing the failed build.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214014639) do
+ActiveRecord::Schema.define(version: 20170216012849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "class_rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
