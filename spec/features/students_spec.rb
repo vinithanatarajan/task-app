60f205d3adfb85059caf532f85a3f073ba767d71
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-02-22 13:33:02 +1300

    Refactored spec by adding a before each block.

--- spec/features/students_spec.rb
+++ spec/features/students_spec.rb
@@ -1,67 +1,61 @@
 require "rails_helper"
 
-RSpec.feature User, type: :feature do
-  let!(:student) { FactoryGirl.create :student }
-  let!(:user) { FactoryGirl.create :user }
+RSpec.feature Student, type: :feature do
 
-  describe "can see list of student" do
-    before do
+  describe "when I visit students page" do
+    let!(:student) { FactoryGirl.create :student }
+    let!(:user) { FactoryGirl.create :user }
+
+    before(:each) do
       sign_in user
       visit "/"
       click_on "Students"
       visit "/students"
     end
-    it "shows the list" do
-      expect(page).to have_text("Students page")
-      expect(page).to have_text(student.name)
-      expect(page).to have_text(student.class_room)
-      expect(page).to have_text(student.current_level)
-    end
-  end
-
-  describe "I can add student" do
-    before do
-      sign_in user
-      visit "/"
-      click_on "Students"
-      visit "/students"
-      click_on "Add a Student"
-      fill_in "Name", with: "Peter"
-      fill_in "Class room", with: "Year 8"
-      fill_in "Current level", with: "At Expected level"
-      click_on "Save"
+    context "see a list of student" do
+      it "shows the list" do
+        expect(page).to have_text("Students page")
+        expect(page).to have_text(student.name)
+        expect(page).to have_text(student.class_room)
+        expect(page).to have_text(student.current_level)
+      end
     end
 
-    it "I can see the new student on the page" do
-      expect(page).to have_content("Peter")
-      expect(page).to have_content("Year 8")
-      expect(page).to have_content("At Expected level")
-    end
-  end
+    context "I can add student" do
+      before do
+        click_on "Add a Student"
+        fill_in "Name", with: "Peter"
+        fill_in "Class room", with: "Year 8"
+        fill_in "Current level", with: "At Expected level"
+        click_on "Save"
+      end
 
-  describe "I can edit the student details" do
-    before do
-      sign_in user
-      visit "/"
-      click_on "Students"
-      visit "/students"
-      click_on "Edit"
-      fill_in "Name", with: "Molly"
-      fill_in "Class room", with: "Year 8"
-      fill_in "Current level", with: "At Expected level"
-      click_on "Save"
+      it "I can see the new student on the page" do
+        expect(page).to have_content("Peter")
+        expect(page).to have_content("Year 8")
+        expect(page).to have_content("At Expected level")
+      end
     end
 
-    it "I can see that the student details are updated" do
-      expect(page).to have_content("Molly")
-      expect(page).to have_content("Year 8")
-      expect(page).to have_content("At Expected level")
+    context "I can edit the student details" do
+      before do
+        click_on "Edit"
+        fill_in "Name", with: "Molly"
+        fill_in "Class room", with: "Year 8"
+        fill_in "Current level", with: "At Expected level"
+        click_on "Save"
+      end
+
+      it "I can see that the student details are updated" do
+        expect(page).to have_content("Molly")
+        expect(page).to have_content("Year 8")
+        expect(page).to have_content("At Expected level")
+      end
     end
-  end
 
   # Commenting the below lines because it fails with Selenium webdriver
 
-  # describe "I can delete the student" do
+  # context "I can delete the student" do
   #   before do
   #     visit "/students"
   #     click_on "Delete"
@@ -73,4 +67,5 @@ RSpec.feature User, type: :feature do
   #     expect(page).to change { Student.count }.by(-1)
   #   end
   # end
+  end
 end
