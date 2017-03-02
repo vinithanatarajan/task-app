c1fb67357ac69d9fa2c372b78294ce9ec630f070
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-03-02 15:13:30 +1300

    Added validations.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ActiveRecord::Base
   has_many :class_rooms
   belongs_to :user
+
+  validates :user, presence: true
+  validates :name, :class_room, :current_level, presence: { message: "Can't be blank" }
 end
