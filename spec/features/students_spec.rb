08786743ac11e9485c5dc6874e861238014b3035
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-03-03 10:13:31 +1300

    Student spec is passing after adding user to the students.

--- spec/features/students_spec.rb
+++ spec/features/students_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature Student, type: :feature do
   describe "when I visit students page" do
-    let!(:student) { FactoryGirl.create :student }
+    let!(:student) { FactoryGirl.create(:student, user: user) }
     let!(:user) { FactoryGirl.create :user }
 
     before(:each) do
@@ -14,9 +14,9 @@ RSpec.feature Student, type: :feature do
     context "see a list of student" do
       it "shows the list" do
         expect(page).to have_text("Students page")
-        # expect(page).to have_text(student.name)
-        # expect(page).to have_text(student.class_room)
-        # expect(page).to have_text(student.current_level)
+        expect(page).to have_text(student.name)
+        expect(page).to have_text(student.class_room)
+        expect(page).to have_text(student.current_level)
       end
     end
 
@@ -31,6 +31,22 @@ RSpec.feature Student, type: :feature do
 
       it "I can see the new student on the page" do
         expect(page).to have_content("Peter")
+        expect(page).to have_content("Year 8")
+        expect(page).to have_content("At Expected level")
+      end
+    end
+
+    context "I can edit the student details" do
+      before do
+        click_on "Edit"
+        fill_in "Name", with: "Molly", visible: false
+        fill_in "Class room", with: "Year 8"
+        fill_in "Current level", with: "At Expected level"
+        click_on "Save"
+      end
+
+      it "I can see that the student details are updated" do
+        expect(page).to have_content("Molly")
         expect(page).to have_content("Year 8")
         expect(page).to have_content("At Expected level")
       end
